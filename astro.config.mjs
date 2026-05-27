7e1f7908aaa4140bbdeb6aa51ebb65c35355282e
IsNotAcceptable <184727191+IsNotAcceptable@users.noreply.github.com>
2026-05-28 02:45:35 +0700

    Start Playing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,35 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
+import markdoc from '@astrojs/markdoc';
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
-				},
-			],
-		}),
-	],
+    integrations: [starlight({
+        title: 'Orehum Sector',
+        social: [
+            { icon: 'github', label: 'GitHub', href: 'https://github.com/Orehum-Project/Orehum-Sector' },
+            { icon: 'discord', label: 'Discord', href: 'https://discord.gg/ZC94VrbFNY' },
+        ],
+        logo: {
+            src: './src/assets/logo.png',
+            replacesTitle: true,
+        },
+        customCss: [
+            './src/styles/custom.css',
+        ],
+        sidebar: [
+            {
+                label: 'Guides',
+                items: [
+                    // Each item here is one entry in the navigation menu.
+                    { label: 'Example Guide', slug: 'guides/example' },
+                ],
+            },
+            {
+                label: 'Reference',
+                items: [{ autogenerate: { directory: 'reference' } }],
+            },
+        ],
+		}), markdoc()],
 });
